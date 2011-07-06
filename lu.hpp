a0464c13346359f0483f3487671791cc6c789b24
Ethan Eade <services@ethaneade.com>
2011-07-06 08:21:16 -0700

    * Add construction operators * Add all arithmetic operators * Cleanup * Start LU

---
+++ lu.hpp
@@ -0,0 +1,75 @@
+// Copyright 2011 Ethan Eade. All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+
+//    1. Redistributions of source code must retain the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer.
+
+//    2. Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials
+//       provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY ETHAN EADE ``AS IS'' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ETHAN EADE OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+
+// The views and conclusions contained in the software and
+// documentation are those of the authors and should not be
+// interpreted as representing official policies, either expressed or
+// implied, of Ethan Eade.
+
+#ifndef LATL_LU_HPP
+#define LATL_LU_HPP
+
+#include <latl/latl.hpp>
+
+namespace latl
+{
+    template <int N, class Scalar>
+    class LU
+    {
+    private:
+        MatrixHolder<N,N,Scalar> lu;
+        VectorHolder<N,Scalar> inv_diag;
+        VectorHolder<N,int> index;
+        int rank;
+        
+    public:
+        LU() {}
+        
+        template <class Mat>
+        LU(const AbstractMatrix<Mat>& m) { compute(m); }
+        
+
+        template <class Mat>
+        void compute(const AbstractMatrix<Mat>& m) {
+            assert_square(m);
+            const int n = m.rows();
+            lu.resize(n, n);
+            assert_same_shape(m, lu());
+            
+            inv_diag.resize(n);
+            index.resize(n);
+
+            for (int i=0; i<n; ++i) {
+                
+            }
+        }
+
+    };
+}
+
+#endif
---
+++ vector_funcs.hpp
@@ -0,0 +1,54 @@
+// Copyright 2011 Ethan Eade. All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+
+//    1. Redistributions of source code must retain the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer.
+
+//    2. Redistributions in binary form must reproduce the above
+//       copyright notice, this list of conditions and the following
+//       disclaimer in the documentation and/or other materials
+//       provided with the distribution.
+
+// THIS SOFTWARE IS PROVIDED BY ETHAN EADE ``AS IS'' AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL ETHAN EADE OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+// OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+// BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+
+// The views and conclusions contained in the software and
+// documentation are those of the authors and should not be
+// interpreted as representing official policies, either expressed or
+// implied, of Ethan Eade.
+
+#ifndef LATL_VECTOR_FUNCS_HPP
+#define LATL_VECTOR_FUNCS_HPP
+
+#include <latl/vector.hpp>
+#include <latl/scalar.hpp>
+
+namespace latl
+{
+    template <class V>
+    LATL_VS(V) norm_sq(const AbstractVector<V>& v) { return v*v; }
+
+    template <class V>
+    Vector<vector_traits<V>::static_size, LATL_VS(V)>
+    unit(const AbstractVector<V>& v) {
+        return v / latl::sqrt(norm_sq(v));
+    }
+    
+    
+}
+
+#endif
